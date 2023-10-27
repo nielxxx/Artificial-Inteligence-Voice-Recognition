6238d10da32495ac1add87ac8d635e68d1621a91
niel <93116556+nielxxx@users.noreply.github.com>
2023-10-27 23:10:49 +0800

    added

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-import openai 
-
-openai.api_key = "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"
-
-def chat_with_gpt(prompt):
-    response = openai.ChatCompletion.create(
-        model="gpt-3-turbo",
-        messages=[{"role": "user", "content": prompt}]
-    )
-
-    return response.choices[0].message.content.strip()
-
-if __name__ == "__main__":
-    while True:
-        user_input = input("You: ")
-        if user_input.lower() in ["quit", "exit", "bye"]:
-            break
-        
-        response = chat_with_gpt(user_input)
-        print("Chatbot:", response)
+from playsound import playsound
+import speech_recognition as sr
+import pyttsx3
+import openai
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine = pyttsx3.init()
+engine.setProperty('rate', 160)
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+def speak(audio):
+    print(str(audio))
+    engine.say(audio)
+    engine.runAndWait()
+openai.api_key = "your api"
+speak('hello niel gwapo, everything good bro? how can I help you today? I gotchu homie')
+while True:
+    try:
+        with sr.Microphone() as source:
+            print('(oo)')
+            audio = sr.Recognizer().listen(source)
+            print("(..)")
+            qwery = sr.Recognizer().recognize_google(audio, language='en-in')
+            word = str(qwery).lower()
+            print(word)   
+            if word == '' or word == ' ':
+                continue
+            response = openai.Completion.create(model="gpt-3.5-turbo-0613", prompt=word, temperature=0, max_tokens=100)
+            res = response['choices'][0]['text']
+            speak(res)
+    except Exception as e:
+        print(e)
